20331f299b567ed6d680aa474e6b7c23933dc8b3
Sensenmann <sensenmann@Sensenmann.zz>
2015-05-15 06:33:36 +0200

    switchetd from raw mode to cbreak mode

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ worldx = 24
 worldy = 80
 worldz = 20
 world = [[[[0 for x in range(3)] for x in range(worldx)] for x in range(worldy)] for x in range(worldz)]
-tty.setraw(sys.stdin)
+tty.setcbreak(sys.stdin)
 os.system('setterm -cursor off')
 	
 while True:
 	line = ""
-	for x in range(worldx):		
+	for x in range(worldx):
 		for y in range(worldy):
 			line += (bc[blocknummer[world[10][y][x][0]].saybg()] + fc[creaturenummer[world[10][y][x][1]].sayfc()] + creaturenummer[world[10][y][x][1]].saychar())
-		if x != worldx - 1:
-			line += ("\r\n")
-		else:
-			line += ("\r")
-	print(line)
+	if x == worldx -1:
+		print(line, end="")
+	else:
+		print(line)
 	ch = sys.stdin.read(1)
 	if ch == "h":
 		world[10][random.randint(0,worldy - 1)][random.randint(0,worldx - 1)][1] = 1
